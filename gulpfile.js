24021abb1baaadba4e4bb2d0f2f27f8f59844085
miraage <miraage.wow@gmail.com>
2015-10-14 23:23:01 +0300

    feat(gulp): gulp base setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,37 @@
 'use strict';
 
-var config = require('./gulp/config');
+//---------------------------------------------------------------------------
+// INIT
+//---------------------------------------------------------------------------
+
+var fs = require('fs');
+
+var config = require('./gulp/_config');
 
 var gulp = require('gulp'),
     $ = require('gulp-load-plugins')(config.plugins);
+
+//---------------------------------------------------------------------------
+// HELPERS
+//---------------------------------------------------------------------------
+
+function getTasks() {
+    var files = fs.readdirSync('./gulp');
+
+    // Remove config
+    files.shift();
+
+    return files.map(function (file) {
+        return file.substring(0, file.indexOf('.'));
+    });
+}
+
+//---------------------------------------------------------------------------
+// RUN
+//---------------------------------------------------------------------------
+
+var tasks = getTasks();
+
+tasks.forEach(function (task) {
+    require('./gulp/' + task)(gulp, $, config[task]);
+});
